5ea3a20ed5d737f8d9f2179ce3103ecb982efa24
Luiz Guilherme Fonseca Rosa <luizguilhermefr@gmail.com>
2018-07-03 15:04:30 -0300

    rearrange, mutate

--- js/GeneticExecutor.js
+++ js/GeneticExecutor.js
@@ -20,6 +20,22 @@ function PictureUtils () {
   }
 }
 
+function Gene (maxX, maxY, start, end) {
+  this.mutate = function (rate) {
+    this.from[0] = Math.floor(((Math.random() < 0.5 ? -1 : 1) * Math.random() * rate) + this.from[0])
+    this.to[0] = Math.floor(((Math.random() < 0.5 ? -1 : 1) * Math.random() * rate) + this.to[0])
+    this.from[1] = Math.floor(((Math.random() < 0.5 ? -1 : 1) * Math.random() * rate) + this.from[1])
+    this.to[1] = Math.floor(((Math.random() < 0.5 ? -1 : 1) * Math.random() * rate) + this.to[1])
+    this.from[0] = this.from[0] > maxX ? maxX : this.from[0]
+    this.to[0] = this.to[0] > maxX ? maxX : this.to[0]
+    this.from[1] = this.from[1] > maxY ? maxY : this.from[1]
+    this.to[1] = this.to[1] > maxY ? maxY : this.to[1]
+  }
+
+  this.from = start
+  this.to = end
+}
+
 function Cromossome (genesQuantity, imgWidth, imgHeight) {
   this.generateGenes = function () {
     for (let i = 0; i < genesQuantity; ++i) {
@@ -36,18 +52,7 @@ function Cromossome (genesQuantity, imgWidth, imgHeight) {
   this.genes = []
 }
 
-function Gene (maxX, maxY, start, end) {
-}
-
 function GeneticExecutor (originalImg, linesQuantity, populationSize, generations, elitism = false) {
-  this.calculateFitness = function () {
-    //
-  }
-
-  this.nextIteration = function () {
-    this.calculateFitness()
-  }
-
   this.executeAll = function (callback) {
     setTimeout(() => {
       this.prepareInput()
@@ -59,14 +64,6 @@ function GeneticExecutor (originalImg, linesQuantity, populationSize, generation
       }
       callback(this.referencePicture)
     }, 0)
-  }
-
-  this.createTargetOutput = function () {
-    const utility = new PictureUtils()
-    this.target = new SimpleImage(this.referencePicture.width, this.referencePicture.height)
-    this.target.pixels().forEach((pixel) => {
-      utility.setPixelWhite(pixel)
-    })
   }
 
   this.prepareInput = function () {
@@ -81,10 +78,12 @@ function GeneticExecutor (originalImg, linesQuantity, populationSize, generation
     })
   }
 
-  this.randomCromossome = function () {
-    const cromossome = new Cromossome(linesQuantity, this.referencePicture.width, this.referencePicture.height)
-    cromossome.generateGenes()
-    return cromossome
+  this.createTargetOutput = function () {
+    const utility = new PictureUtils()
+    this.target = new SimpleImage(this.referencePicture.width, this.referencePicture.height)
+    this.target.pixels().forEach((pixel) => {
+      utility.setPixelWhite(pixel)
+    })
   }
 
   this.generateInitialPopulation = function () {
@@ -93,8 +92,22 @@ function GeneticExecutor (originalImg, linesQuantity, populationSize, generation
     }
   }
 
+  this.randomCromossome = function () {
+    const cromossome = new Cromossome(linesQuantity, this.referencePicture.width, this.referencePicture.height)
+    cromossome.generateGenes()
+    return cromossome
+  }
+
   this.hasNextIteration = function () {
     return this.currentIteration < this.generations
+  }
+
+  this.nextIteration = function () {
+    this.calculateFitness()
+  }
+
+  this.calculateFitness = function () {
+    //
   }
 
   this.target = null
