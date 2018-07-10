3c3951a78ee97a28ace14a51352b612c474cb718
Luiz Guilherme Fonseca Rosa <luizguilhermefr@gmail.com>
2018-07-10 04:35:48 -0300

    pode isso arnaldo

--- js/GeneticExecutor.js
+++ js/GeneticExecutor.js
@@ -1,3 +1,9 @@
+function RandomUtils () {
+  this.randomInteger = function (min, max) {
+    return Math.floor(Math.random() * max) + min
+  }
+}
+
 function PictureUtils () {
   this.calculateNtscY = function (pixel) {
     return 0.299 * pixel.getRed() + 0.587 * pixel.getGreen() + 0.114 * pixel.getBlue()
@@ -92,25 +98,33 @@ function PictureUtils () {
 
 function Gene (maxX, maxY, start, end) {
   this.mutate = function (rate) {
-    this.from[0] = Math.floor(((Math.random() < 0.5 ? -1 : 1) * Math.random() * rate) + this.from[0])
-    this.to[0] = Math.floor(((Math.random() < 0.5 ? -1 : 1) * Math.random() * rate) + this.to[0])
-    this.from[1] = Math.floor(((Math.random() < 0.5 ? -1 : 1) * Math.random() * rate) + this.from[1])
-    this.to[1] = Math.floor(((Math.random() < 0.5 ? -1 : 1) * Math.random() * rate) + this.to[1])
+    const randomUtils = new RandomUtils()
+    this.from[0] = randomUtils.randomInteger(this.from[0] - rate, this.from[0] + rate)
+    this.from[1] = randomUtils.randomInteger(this.from[1] - rate, this.from[1] + rate)
+    this.to[0] = randomUtils.randomInteger(this.to[0] - rate, this.from[0] + rate)
+    this.to[1] = randomUtils.randomInteger(this.to[1] - rate, this.from[1] + rate)
     this.from[0] = this.from[0] > maxX ? maxX : this.from[0]
-    this.to[0] = this.to[0] > maxX ? maxX : this.to[0]
     this.from[1] = this.from[1] > maxY ? maxY : this.from[1]
+    this.to[0] = this.to[0] > maxX ? maxX : this.to[0]
     this.to[1] = this.to[1] > maxY ? maxY : this.to[1]
   }
 
   this.from = start
+
   this.to = end
 }
 
-function Cromossome (genesQuantity, imgWidth, imgHeight) {
+function Cromossome (genesQuantity, imgWidth, imgHeight, initialGenes = []) {
   this.generateGenes = function () {
     for (let i = 0; i < genesQuantity; ++i) {
       this.genes.push(this.randomGene())
     }
+  }
+
+  this.mutate = function (rate) {
+    this.genes.forEach((gene) => {
+      gene.mutate(rate)
+    })
   }
 
   this.randomGene = function () {
@@ -119,8 +133,8 @@ function Cromossome (genesQuantity, imgWidth, imgHeight) {
     return new Gene(imgWidth, imgHeight, from, to)
   }
 
-  this.equals = function (other) {
-    return this.genes === other.genes
+  this.getPartialGenes = function (fromIndex, endIndex) {
+    return this.genes.slice(fromIndex, endIndex + 1)
   }
 
   this.toPixelMatrix = function () {
@@ -156,7 +170,7 @@ function Cromossome (genesQuantity, imgWidth, imgHeight) {
     return simpleImage
   }
 
-  this.genes = []
+  this.genes = initialGenes
 }
 
 function GeneticExecutor (originalImg, linesQuantity, populationSize, generations, elitism = false) {
@@ -164,10 +178,10 @@ function GeneticExecutor (originalImg, linesQuantity, populationSize, generation
     setTimeout(() => {
       this.prepareInput()
       this.generateInitialPopulation()
-      // while (this.hasNextIteration()) {
-      this.nextIteration()
-      this.currentIteration++
-      // }
+      while (this.hasNextIteration()) {
+        this.nextIteration()
+        this.currentIteration++
+      }
       callback(this.bestCromossomeOfAllTimes.toSimpleImage())
     }, 0)
   }
@@ -202,18 +216,42 @@ function GeneticExecutor (originalImg, linesQuantity, populationSize, generation
   }
 
   this.hasNextIteration = function () {
-    return this.currentIteration < generations
+    return this.currentIteration <= generations
   }
 
   this.nextIteration = function () {
     this.calculateFitnessForEveryCromossome()
     const first = this.roulette()
     let second = this.roulette()
-    // while (second.equals(first)) {
-    //   second = this.roulette()
-    //   console.log('aaaaaaaaaaaaaaa')
-    // }
-    this.bestCromossomeOfAllTimes = second
+    while (first === second) {
+      second = this.roulette()
+    }
+    const firstSon = this.crossover(first, second)
+    const secondSon = this.crossover(second, first)
+    this.currentPopulation.filter((cromossome) => cromossome !== first && cromossome !== second)
+    this.currentPopulation.push(firstSon)
+    this.currentPopulation.push(secondSon)
+    this.mutateEveryCromossome()
+    const bestOfThisIteration = this.bestCromossomeOfCurrentGeneration()
+    if (this.bestCromossomeOfAllTimes === null || bestOfThisIteration.latestFitness > this.bestCromossomeOfAllTimes.latestFitness) {
+      this.bestCromossomeOfAllTimes = Object.assign(Object.create(Object.getPrototypeOf(bestOfThisIteration)), bestOfThisIteration)
+    }
+  }
+
+  this.mutateEveryCromossome = function () {
+    this.currentPopulation.forEach((cromossome) => {
+      cromossome.mutate(this.mutationRate)
+    })
+  }
+
+  this.crossover = function (male, female) {
+    const half = linesQuantity / 2
+    const tolerance = linesQuantity / 6
+    const crosspoint = Math.floor((Math.random() * (half - tolerance)) + tolerance)
+    const halfOfMale = male.getPartialGenes(0, crosspoint)
+    const halfOfFemale = female.getPartialGenes(crosspoint + 1, linesQuantity - 1)
+    const resultant = [].concat(halfOfFemale).concat(halfOfMale)
+    return new Cromossome(linesQuantity, originalImg.width, originalImg.height, resultant)
   }
 
   this.roulette = function () {
@@ -239,8 +277,8 @@ function GeneticExecutor (originalImg, linesQuantity, populationSize, generation
     this.currentPopulation.forEach((cromossome) => {
       const pictureMatrix = cromossome.toPixelMatrix()
       let fitness = 0
-      for (let y = 0; y < this.referencePicture.height; y++) {
-        for (let x = 0; x < this.referencePicture.width; x++) {
+      for (let y = 0; y < originalImg.height; y++) {
+        for (let x = 0; x < originalImg.width; x++) {
           const originalPixel = this.referencePicture[y][x]
           const synteticPixel = pictureMatrix[y][x]
           if (originalPixel === synteticPixel) {
@@ -253,6 +291,20 @@ function GeneticExecutor (originalImg, linesQuantity, populationSize, generation
       cromossome.latestFitness = fitness
     })
   }
+
+  this.bestCromossomeOfCurrentGeneration = function () {
+    let bestFitness = Number.MIN_SAFE_INTEGER
+    let bestCromossome = null
+    this.currentPopulation.forEach((cromossome) => {
+      if (cromossome.latestFitness > bestFitness) {
+        bestCromossome = cromossome
+        bestFitness = cromossome.latestFitness
+      }
+    })
+    return bestCromossome
+  }
+
+  this.mutationRate = 5
 
   this.bestCromossomeOfAllTimes = null
 
