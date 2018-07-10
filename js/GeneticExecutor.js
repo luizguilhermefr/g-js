008a4ee07d3bf8c3bf289c5c0fce8646ced23259
Luiz Guilherme Fonseca Rosa <luizguilhermefr@gmail.com>
2018-07-10 02:54:07 -0300

    commit

--- js/GeneticExecutor.js
+++ js/GeneticExecutor.js
@@ -19,18 +19,69 @@ function PictureUtils () {
     pixel.setBlue(this.BLACK)
   }
 
+  this.setPixelGrayScale = function (pixel, scale) {
+    pixel.setRed(scale)
+    pixel.setGreen(scale)
+    pixel.setBlue(scale)
+  }
+
+  this.vLine = function ({matrix, x, y0, y1}) {
+    const ya = Math.round(y0)
+    const yb = Math.round(y1)
+    const y = ya < yb ? ya : yb
+    for (let i = Math.abs(ya - yb); i >= 0; i--) {
+      matrix[y + i][x] = this.BLACK
+    }
+  }
+
+  this.hLine = function ({matrix, y, x0, x1}) {
+    const xa = Math.round(x0)
+    const xb = Math.round(x1)
+    const x = xa < xb ? xa : xb
+    for (let i = Math.abs(xa - xb); i >= 0; i--) {
+      matrix[y][x + i] = this.BLACK
+    }
+  }
+
   this.drawLine = function ({matrix, x0, y0, x1, y1}) {
-    const dx = x1 - x0
-    const dy = y1 - y0
-    const steps = Math.abs(dx) > Math.abs(dy) ? Math.abs(dx) : Math.abs(dy)
-    const xInc = dx / steps
-    const yInc = dy / steps
-    let x = x0
-    let y = y0
-    for (let i = 0; i <= steps; i++) {
-      matrix[y][x] = this.BLACK
-      x += xInc
-      y += yInc
+    let xa = Math.round(x0)
+    let ya = Math.round(y0)
+    let xb = Math.round(x1)
+    let yb = Math.round(y1)
+    let dx = xb - xa
+    let dy = yb - ya
+    let cInc, rInc, h, i
+    if (dx === 0) return this.vLine({matrix, x: x0, y0, y1})
+    else if (dy === 0) return this.hLine({matrix, y: y0, x0, x1})
+    cInc = dx < 0 ? -1 : 1
+    rInc = dy < 0 ? -1 : 1
+    dx = Math.abs(dx)
+    dy = Math.abs(dy)
+    if (dx >= dy) {
+      h = -dx
+      matrix[ya][xa] = this.BLACK
+      for (i = dx; i > 0; i--) {
+        xa += cInc
+        h += 2 * dy
+        if (h >= 0) {
+          ya += rInc
+          h -= 2 * dx
+        }
+        matrix[ya][xa] = this.BLACK
+      }
+
+    } else {
+      h = -dy
+      matrix[ya][xa] = this.BLACK
+      for (i = dy; i > 0; i--) {
+        ya += rInc
+        h += 2 * dx
+        if (h >= 0) {
+          xa += cInc
+          h -= 2 * dy
+        }
+        matrix[ya][xa] = this.BLACK
+      }
     }
   }
 
@@ -91,7 +142,19 @@ function Cromossome (genesQuantity, imgWidth, imgHeight) {
     return matrix
   }
 
-  this.latestFitness = 0
+  this.toSimpleImage = function () {
+    const utility = new PictureUtils()
+    const matrix = this.toPixelMatrix()
+    const simpleImage = new SimpleImage(imgWidth, imgHeight)
+    for (let y = 0; y < imgHeight; y++) {
+      for (let x = 0; x < imgWidth; x++) {
+        const synteticPixel = matrix[y][x]
+        const originalPixel = simpleImage.getPixel(x, y)
+        utility.setPixelGrayScale(originalPixel, synteticPixel)
+      }
+    }
+    return simpleImage
+  }
 
   this.genes = []
 }
@@ -101,11 +164,11 @@ function GeneticExecutor (originalImg, linesQuantity, populationSize, generation
     setTimeout(() => {
       this.prepareInput()
       this.generateInitialPopulation()
-      while (this.hasNextIteration()) {
-        this.nextIteration()
-        this.currentIteration++
-      }
-      callback(this.referencePicture)
+      // while (this.hasNextIteration()) {
+      this.nextIteration()
+      this.currentIteration++
+      // }
+      callback(this.bestCromossomeOfAllTimes.toSimpleImage())
     }, 0)
   }
 
@@ -143,25 +206,28 @@ function GeneticExecutor (originalImg, linesQuantity, populationSize, generation
   }
 
   this.nextIteration = function () {
+    this.calculateFitnessForEveryCromossome()
     const first = this.roulette()
     let second = this.roulette()
-    while (second.equals(first)) {
-      second = this.roulette()
-    }
+    // while (second.equals(first)) {
+    //   second = this.roulette()
+    //   console.log('aaaaaaaaaaaaaaa')
+    // }
+    this.bestCromossomeOfAllTimes = second
   }
 
   this.roulette = function () {
     const fitnessSum = this.currentPopulation.reduce((previousValue, cromossome) => {
-      return this.calculateFitness(cromossome) + previousValue
+      return cromossome.latestFitness + previousValue
     }, 0)
     const rndDrawn = Math.random() * fitnessSum
     let accumulated = 0
     let chosen = null
-    this.currentPopulation.every((el) => {
+    this.currentPopulation.every((cromossome) => {
       const accumulatedBeforeSum = accumulated
-      accumulated += el.latestFitness
+      accumulated += cromossome.latestFitness
       if (accumulatedBeforeSum <= rndDrawn && accumulated >= rndDrawn) {
-        chosen = el
+        chosen = cromossome
         return false // break
       }
       return true // continue
@@ -169,25 +235,26 @@ function GeneticExecutor (originalImg, linesQuantity, populationSize, generation
     return chosen
   }
 
-  this.calculateFitness = function (cromossome) {
-    const pictureMatrix = cromossome.toPixelMatrix()
-    let fitness = 0
-    for (let y = 0; y < this.referencePicture.height; y++) {
-      for (let x = 0; x < this.referencePicture.width; x++) {
-        const originalPixel = this.referencePicture[y][x]
-        const synteticPixel = pictureMatrix[y][x]
-        if (originalPixel === synteticPixel) {
-          fitness++
-        } else {
-          fitness--
+  this.calculateFitnessForEveryCromossome = function () {
+    this.currentPopulation.forEach((cromossome) => {
+      const pictureMatrix = cromossome.toPixelMatrix()
+      let fitness = 0
+      for (let y = 0; y < this.referencePicture.height; y++) {
+        for (let x = 0; x < this.referencePicture.width; x++) {
+          const originalPixel = this.referencePicture[y][x]
+          const synteticPixel = pictureMatrix[y][x]
+          if (originalPixel === synteticPixel) {
+            fitness++
+          } else {
+            fitness--
+          }
         }
       }
-    }
-    cromossome.latestFitness = fitness
-    return fitness
+      cromossome.latestFitness = fitness
+    })
   }
 
-  this.target = null
+  this.bestCromossomeOfAllTimes = null
 
   this.currentPopulation = []
 
